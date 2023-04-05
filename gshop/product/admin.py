e6825a14c66f20846ad2d060881b87e2907950c8
iliaziuz <tiekhmurov@gmail.com>
2023-04-05 13:14:29 +0300

    admin patch

--- gshop/product/admin.py
+++ gshop/product/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImages
 
 
-admin.site.register(Product)
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImages
+    extra = 1  # кількість додаткових полів для завантаження зображень
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesInline]
+
+
+admin.site.register(Product, ProductAdmin)
